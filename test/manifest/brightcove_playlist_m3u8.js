f71a8576e28e43c2f5bcfeb33fdcdcd1f7807a17
David LaPalomento <dlapalomento@gmail.com>
2013-12-22 18:39:24 -0500

    Convert m3u8 tests to use deepEquals against a known "good" parse Take all the remaining test cases and conver them into pairs of m3u8 and json files. Removed handlebars since it's no longer in use.

--- test/manifest/brightcove_playlist_m3u8.js
+++
@@ -1,9 +0,0 @@
-window.brightcove_playlist_data = '#EXTM3U\n'+
-'#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=240000,RESOLUTION=396x224\n'+
-'http://c.brightcove.com/services/mobile/streaming/index/rendition.m3u8?assetId=1824686811001&videoId=1824650741001\n'+
-'#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=40000\n'+
-'http://c.brightcove.com/services/mobile/streaming/index/rendition.m3u8?assetId=1824683759001&videoId=1824650741001\n'+
-'#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=440000,RESOLUTION=396x224\n'+
-'http://c.brightcove.com/services/mobile/streaming/index/rendition.m3u8?assetId=1824686593001&videoId=1824650741001\n'+
-'#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=1928000,RESOLUTION=960x540\n'+
-'http://c.brightcove.com/services/mobile/streaming/index/rendition.m3u8?assetId=1824687660001&videoId=1824650741001';
