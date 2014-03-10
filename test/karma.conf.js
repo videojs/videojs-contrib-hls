ad0833bad397be715a987b139016fd7c0ec83ae8
Parul Sharma <psharma@brightcove.com>
2014-03-10 11:00:26 -0400

    added IRC

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -79,14 +79,11 @@ module.exports = function(config) {
     // web server port
     port: 9876,
 
-
     // cli runner port
     runnerPort: 9100,
 
-
     // enable / disable colors in the output (reporters and logs)
     colors: true,
-
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
