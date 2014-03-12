bfc5bfcbafa2eb09c26032ffa93e609573be5955
Parul Sharma <psharma@brightcove.com>
2014-03-12 15:16:02 -0400

    attempt at fixing travisbuild fails

--- Gruntfile.js
+++ Gruntfile.js
@@ -262,7 +262,9 @@ module.exports = function(grunt) {
                       ['manifests-to-js', 'qunit', 'karma']);
 
   // travis build task
-  grunt.registerTask('build:travis', ['jshint', 'test:node', 'test:sauce-browser']);
+  grunt.registerTask('build:travis', ['jshint', 'test:node', 'test:sauceLabs']);
+
+  //test:sauce-browser
 
 
 };
