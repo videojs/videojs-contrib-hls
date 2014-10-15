4bf4997ad260ecb9eaee4fd1a53a4c1c61fa2aa3
Tom Johnson <seniorflexdeveloper@gmail.com>
2014-10-15 06:42:56 -0700

    update get switch buffer calculation

--- src/videojs-hls.js
+++ src/videojs-hls.js
@@ -210,23 +210,18 @@ videojs.Hls.prototype.handleSourceOpen = function() {
  * @returns Number Time in milliseconds
  */
 videojs.Hls.prototype.getSwitchBuffer = function() {
-  var tech, timeRanges, playerCurrentTimeInMilliseconds,
-    currentSegmentStartTime, nextSegmentDuration, currentIndex;
+  var tech, timeRanges, playerCurrentTimeInMilliseconds, currentSegmentStartTime,
+    currentSegmentRemainingPlaybackTime, nextSegmentDuration, currentIndex;
 
   tech = this;
   timeRanges = videojs.Hls.getSegmentTimeRangesByPlaylist(this.playlists.media());
-
   playerCurrentTimeInMilliseconds = tech.player().currentTime() * 1000;
-
-  currentIndex = tech.mediaIndex - 1;
-  if (currentIndex < 0) {
-    currentIndex = 0;
-  }
-
+  currentIndex = Math.max(tech.mediaIndex-1,0);
   currentSegmentStartTime = timeRanges[currentIndex].start;
+  currentSegmentRemainingPlaybackTime = playerCurrentTimeInMilliseconds - currentSegmentStartTime;
   nextSegmentDuration = timeRanges[currentIndex + 1].end - timeRanges[currentIndex + 1].start;
 
-  return playerCurrentTimeInMilliseconds + currentSegmentStartTime + nextSegmentDuration;
+  return currentSegmentRemainingPlaybackTime + nextSegmentDuration;
 };
 
 /**
