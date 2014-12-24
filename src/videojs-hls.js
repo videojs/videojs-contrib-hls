da3c4e54c95c66d308e434b246ea9eca1a2cec33
dista <dista@qq.com>
2014-12-25 01:11:13 +0800

    use original comment

--- src/videojs-hls.js
+++ src/videojs-hls.js
@@ -828,7 +828,7 @@ videojs.Hls.translateMediaIndex = function(mediaIndex, original, update) {
     return videojs.Hls.setMediaIndexForLive(update) + 1;
   }
 
-  // sync
+  // sync on media sequence
   return translatedMediaIndex;
 };
 
