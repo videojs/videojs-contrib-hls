140c081008a1166d56956d86d6fcb00330796f33
Brandon Bay <bbay@brightcove.com>
2015-03-12 15:48:43 -0400

    Live start time tweaks
    Moving live start time calculation earlier in the code flow to
    eliminate a race condition, repeating it on first play to create a
    smooth experience, and making sure currentTime is updated to match
    these changes.

--- src/videojs-hls.js
+++ src/videojs-hls.js
@@ -164,6 +164,16 @@ videojs.Hls.prototype.src = function(src) {
       });
     }
 
+    // Start live playlists 30 seconds before the current time
+    // This is done using the old playlist because of a race condition
+    // where the playlist selected below may not be loaded quickly
+    // enough to have its segments available for review. When we receive
+    // a loadedplaylist event, we will call translateMediaIndex and
+    // maintain our position at the live point.
+    if (this.duration() === Infinity && this.mediaIndex === 0) {
+      this.mediaIndex = videojs.Hls.getMediaIndexForLive(oldMediaPlaylist);
+    }
+
     selectedPlaylist = this.selectPlaylist();
     oldBitrate = oldMediaPlaylist.attributes &&
                  oldMediaPlaylist.attributes.BANDWIDTH || 0;
@@ -177,11 +187,6 @@ videojs.Hls.prototype.src = function(src) {
 
     if (!segmentDlTime) {
       segmentDlTime = Infinity;
-    }
-
-    // start live playlists 30 seconds before the current time
-    if (this.duration() === Infinity && this.mediaIndex === 0 && selectedPlaylist.segments) {
-      this.mediaIndex = videojs.Hls.setMediaIndexForLive(selectedPlaylist);
     }
 
     // this threshold is to account for having a high latency on the manifest
@@ -239,7 +244,10 @@ videojs.Hls.prototype.src = function(src) {
   });
 };
 
-videojs.Hls.setMediaIndexForLive = function(selectedPlaylist) {
+/* Returns the media index for the live point in the current playlist, and updates
+   the current time to go along with it.
+ */
+videojs.Hls.getMediaIndexForLive = function(selectedPlaylist) {
   var tailIterator = selectedPlaylist.segments.length,
       tailDuration = 0,
       targetTail = (selectedPlaylist.targetDuration || 10) * 3;
@@ -277,6 +285,11 @@ videojs.Hls.prototype.handleSourceOpen = function() {
 videojs.Hls.prototype.play = function() {
   if (this.ended()) {
     this.mediaIndex = 0;
+  }
+
+  if (this.playlists.media() && !this.el().classList.contains('vjs-has-started')) {
+    this.mediaIndex = videojs.Hls.getMediaIndexForLive(this.playlists.media());
+    this.setCurrentTime(this.setCurrentTimeByMediaIndex(this.playlists.media(), this.mediaIndex));
   }
 
   // delegate back to the Flash implementation
@@ -936,7 +949,7 @@ videojs.Hls.translateMediaIndex = function(mediaIndex, original, update) {
 
   if (translatedMediaIndex >= update.segments.length || translatedMediaIndex < 0) {
     // recalculate the live point if the streams are too far out of sync
-    return videojs.Hls.setMediaIndexForLive(update) + 1;
+    return videojs.Hls.getMediaIndexForLive(update) + 1;
   }
 
   // sync on media sequence
@@ -971,6 +984,20 @@ videojs.Hls.getMediaIndexByTime = function(playlist, time) {
   }
 
   return -1;
+};
+
+videojs.Hls.prototype.setCurrentTimeByMediaIndex = function(playlist, mediaIndex) {
+  var index, time = 0;
+
+  if (!playlist.segments || mediaIndex === 0) {
+    return 0;
+  }
+
+  for (index = 0; index < mediaIndex - 1; index++) {
+    time += playlist.segments[index].duration;
+  }
+
+  return time;
 };
 
 /**
