bb12d03f91e085342b448995243704a5ba0c22d1
Josep Vinyals <jvquarck@gmail.com>
2015-12-03 11:30:55 +0100

    Add readyState check on seeking (setCurrentTime)

--- src/videojs-hls.js
+++ src/videojs-hls.js
@@ -434,7 +434,7 @@ videojs.Hls.prototype.setCurrentTime = function(currentTime) {
   this.mediaIndex = this.playlists.getMediaIndexForTime_(currentTime);
 
   // abort any segments still being decoded
-  if (this.sourceBuffer) {
+  if (this.sourceBuffer && this.mediaSource.readyState === 'open') {
     this.sourceBuffer.abort();
   }
 
