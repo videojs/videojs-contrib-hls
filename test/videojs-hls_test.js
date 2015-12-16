53210b254627942deadad8e0923175a8c1a5ae86
jrivera <jrivera@brightcove.com>
2015-12-16 14:23:39 -0500

    Fix tests readyState should have been a function all along added a test for expired seconds tracking not beginning until firstplay is triggered

--- test/videojs-hls_test.js
+++ test/videojs-hls_test.js
@@ -335,7 +335,7 @@ test('autoplay seeks to the live point after playlist load', function() {
     type: 'application/vnd.apple.mpegurl'
   });
   openMediaSource(player);
-  player.tech_.readyState = 3;
+  player.tech_.readyState = function(){return 1;};
   player.tech_.trigger('play');
   standardXHRResponse(requests.shift());
   clock.tick(1);
@@ -357,7 +357,7 @@ test('autoplay seeks to the live point after media source open', function() {
   clock.tick(1);
   standardXHRResponse(requests.shift());
   openMediaSource(player);
-  player.tech_.readyState = 3;
+  player.tech_.readyState = function(){return 1;};
   player.tech_.trigger('play');
   clock.tick(1);
 
@@ -410,7 +410,7 @@ test('calls `remove` on sourceBuffer to when loading a live segment', function()
   player.tech_.hls.playlists.trigger('loadedmetadata');
   player.tech_.trigger('canplay');
   player.tech_.paused = function() { return false; };
-  player.tech_.readyState = 3;
+  player.tech_.readyState = function(){return 1;};
   player.tech_.trigger('play');
 
   clock.tick(1);
@@ -1688,7 +1688,7 @@ test('live playlist starts three target durations before live', function() {
   equal(requests.length, 0, 'no outstanding segment request');
 
   player.tech_.paused = function() { return false; };
-  player.tech_.readyState = 3;
+  player.tech_.readyState = function(){return 1;};
   player.tech_.trigger('play');
   clock.tick(1);
   mediaPlaylist = player.tech_.hls.playlists.media();
@@ -1709,7 +1709,7 @@ test('live playlist starts with correct currentTime value', function() {
   player.tech_.hls.playlists.trigger('loadedmetadata');
 
   player.tech_.paused = function() { return false; };
-  player.tech_.readyState = 3;
+  player.tech_.readyState = function(){return 1;};
   player.tech_.trigger('play');
   clock.tick(1);
 
