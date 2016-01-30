ce399e1c36bae7a6411e60c7b8899e5b08589613
Chunliang Lyu <hi@chunlianglyu.com>
2016-01-31 00:03:20 +0800

    Fix the parsing of the ZEN-TOTAL-DURATION tag

--- src/m3u8/m3u8-parser.js
+++ src/m3u8/m3u8-parser.js
@@ -126,7 +126,7 @@
     }
 
     // Comments
-    if (line.indexOf('#EXT') !== 0) {
+    if (line.indexOf('#EXT') !== 0 && line.indexOf('#ZEN-TOTAL-DURATION:') !== 0) {
       this.trigger('data', {
         type: 'comment',
         text: line.slice(1)
