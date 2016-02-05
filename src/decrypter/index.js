d6b1c19533582d6b590789de03f2d0c3e2fa82b1
brandonocasey <brandonocasey@gmail.com>
2016-02-05 14:37:01 -0500

    added file-level comments to files

--- src/decrypter/index.js
+++ src/decrypter/index.js
@@ -1,3 +1,12 @@
+/**
+ * Main file for Decrypter so that we can neatly export
+ * all of our functionality as one object in one file
+ * and we will only have to require the top level directory
+ * in this case that would look like:
+ * ``` JavaScript
+ * import {Decrypter, decrypt, AsyncStream} from './src/decrypter';
+ * ```
+ */
 import {decrypt, Decrypter} from './decrypter';
 import AsyncStream from './async-stream';
 
