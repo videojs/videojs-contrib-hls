a9d3a5d6c6dd91f1a1815d926fda1cac91e410b6
brandonocasey <brandonocasey@gmail.com>
2016-02-17 15:32:02 -0500

    refactor/better es6 of m3u8 parser

--- src/m3u8/parser.js
+++ src/m3u8/parser.js
@@ -1,8 +1,3 @@
-import Stream from '../stream' ;
-import LineStream from './line-stream';
-import ParseStream from './parse-stream';
-import {mergeOptions} from 'video.js';
-
 /**
  * A parser for M3U8 files. The current interpretation of the input is
  * exposed as a property `manifest` on parser objects. It's just two lines to
@@ -21,19 +16,20 @@ import {mergeOptions} from 'video.js';
  * events during the parse if it encounters input that seems invalid or
  * requires some property of the manifest object to be defaulted.
  */
+import Stream from '../stream' ;
+import LineStream from './line-stream';
+import ParseStream from './parse-stream';
+import {mergeOptions} from 'video.js';
+
 export default class Parser extends Stream {
   constructor() {
     super();
     this.lineStream = new LineStream();
     this.parseStream = new ParseStream();
     this.lineStream.pipe(this.parseStream);
-    /* eslint-disable consistent-this */
-    let self = this;
-    /* eslint-enable consistent-this */
-    let uris = [];
-    let currentUri = {};
-    let key;
-    let noop = function() {};
+    this.uris = [];
+    this.currentUri = {};
+    this.key = null;
 
     // the manifest is empty until the parse stream begins delivering data
     this.manifest = {
@@ -42,189 +38,201 @@ export default class Parser extends Stream {
     };
 
     // update the manifest with the m3u8 entry from the parse stream
-    this.parseStream.on('data', function(entry) {
-      ({
-        tag() {
-          // switch based on the tag type
-          (({
-            'allow-cache'() {
-              this.manifest.allowCache = entry.allowed;
-              if (!('allowed' in entry)) {
-                this.trigger('info', {
-                  message: 'defaulting allowCache to YES'
-                });
-                this.manifest.allowCache = true;
-              }
-            },
-            byterange() {
-              let byterange = {};
-
-              if ('length' in entry) {
-                currentUri.byterange = byterange;
-                byterange.length = entry.length;
-
-                if (!('offset' in entry)) {
-                  this.trigger('info', {
-                    message: 'defaulting offset to zero'
-                  });
-                  entry.offset = 0;
-                }
-              }
-              if ('offset' in entry) {
-                currentUri.byterange = byterange;
-                byterange.offset = entry.offset;
-              }
-            },
-            endlist() {
-              this.manifest.endList = true;
-            },
-            inf() {
-              if (!('mediaSequence' in this.manifest)) {
-                this.manifest.mediaSequence = 0;
-                this.trigger('info', {
-                  message: 'defaulting media sequence to zero'
-                });
-              }
-              if (!('discontinuitySequence' in this.manifest)) {
-                this.manifest.discontinuitySequence = 0;
-                this.trigger('info', {
-                  message: 'defaulting discontinuity sequence to zero'
-                });
-              }
-              if (entry.duration >= 0) {
-                currentUri.duration = entry.duration;
-              }
-
-              this.manifest.segments = uris;
-
-            },
-            key() {
-              if (!entry.attributes) {
-                this.trigger('warn', {
-                  message: 'ignoring key declaration without attribute list'
-                });
-                return;
-              }
-              // clear the active encryption key
-              if (entry.attributes.METHOD === 'NONE') {
-                key = null;
-                return;
-              }
-              if (!entry.attributes.URI) {
-                this.trigger('warn', {
-                  message: 'ignoring key declaration without URI'
-                });
-                return;
-              }
-              if (!entry.attributes.METHOD) {
-                this.trigger('warn', {
-                  message: 'defaulting key method to AES-128'
-                });
-              }
-
-              // setup an encryption key for upcoming segments
-              key = {
-                method: entry.attributes.METHOD || 'AES-128',
-                uri: entry.attributes.URI
-              };
-
-              if (typeof entry.attributes.IV !== 'undefined') {
-                key.iv = entry.attributes.IV;
-              }
-            },
-            'media-sequence'() {
-              if (!isFinite(entry.number)) {
-                this.trigger('warn', {
-                  message: 'ignoring invalid media sequence: ' + entry.number
-                });
-                return;
-              }
-              this.manifest.mediaSequence = entry.number;
-            },
-            'discontinuity-sequence'() {
-              if (!isFinite(entry.number)) {
-                this.trigger('warn', {
-                  message: 'ignoring invalid discontinuity sequence: ' + entry.number
-                });
-                return;
-              }
-              this.manifest.discontinuitySequence = entry.number;
-            },
-            'playlist-type'() {
-              if (!(/VOD|EVENT/).test(entry.playlistType)) {
-                this.trigger('warn', {
-                  message: 'ignoring unknown playlist type: ' + entry.playlist
-                });
-                return;
-              }
-              this.manifest.playlistType = entry.playlistType;
-            },
-            'stream-inf'() {
-              this.manifest.playlists = uris;
-
-              if (!entry.attributes) {
-                this.trigger('warn', {
-                  message: 'ignoring empty stream-inf attributes'
-                });
-                return;
-              }
-
-              if (!currentUri.attributes) {
-                currentUri.attributes = {};
-              }
-              currentUri.attributes = mergeOptions(currentUri.attributes,
-                                                   entry.attributes);
-            },
-            discontinuity() {
-              currentUri.discontinuity = true;
-              this.manifest.discontinuityStarts.push(uris.length);
-            },
-            targetduration() {
-              if (!isFinite(entry.duration) || entry.duration < 0) {
-                this.trigger('warn', {
-                  message: 'ignoring invalid target duration: ' + entry.duration
-                });
-                return;
-              }
-              this.manifest.targetDuration = entry.duration;
-            },
-            totalduration() {
-              if (!isFinite(entry.duration) || entry.duration < 0) {
-                this.trigger('warn', {
-                  message: 'ignoring invalid total duration: ' + entry.duration
-                });
-                return;
-              }
-              this.manifest.totalDuration = entry.duration;
-            }
-          })[entry.tagType] || noop).call(self);
-        },
-        uri() {
-          currentUri.uri = entry.uri;
-          uris.push(currentUri);
-
-          // if no explicit duration was declared, use the target duration
-          if (this.manifest.targetDuration &&
-              !('duration' in currentUri)) {
-            this.trigger('warn', {
-              message: 'defaulting segment duration to the target duration'
-            });
-            currentUri.duration = this.manifest.targetDuration;
-          }
-          // annotate with encryption information, if necessary
-          if (key) {
-            currentUri.key = key;
-          }
-
-          // prepare for the next URI
-          currentUri = {};
-        },
-        comment() {
-          // comments are not important for playback
-        }
-      })[entry.type].call(self);
+    this.parseStream.on('data', (entry) => {
+      switch (entry.type) {
+      case 'tag':
+        this.tag_(entry);
+        break;
+      case 'uri':
+        this.uri_(entry);
+        break;
+      case 'comment':
+        this.comment_(entry);
+        break;
+      default:
+        break;
+      }
     });
+  }
 
+  tag_(entry) {
+    // switch based on the tag type
+    switch (entry.tagType) {
+    case 'allow-cache':
+      this.manifest.allowCache = entry.allowed;
+      if (!('allowed' in entry)) {
+        this.trigger('info', {
+          message: 'defaulting allowCache to YES'
+        });
+        this.manifest.allowCache = true;
+      }
+      break;
+    case 'byterange':
+      let byterange = {};
+
+      if ('length' in entry) {
+        this.currentUri.byterange = byterange;
+        byterange.length = entry.length;
+
+        if (!('offset' in entry)) {
+          this.trigger('info', {
+            message: 'defaulting offset to zero'
+          });
+          entry.offset = 0;
+        }
+      }
+      if ('offset' in entry) {
+        this.currentUri.byterange = byterange;
+        byterange.offset = entry.offset;
+      }
+      break;
+    case 'endlist':
+      this.manifest.endList = true;
+      break;
+    case 'inf':
+      if (!('mediaSequence' in this.manifest)) {
+        this.manifest.mediaSequence = 0;
+        this.trigger('info', {
+          message: 'defaulting media sequence to zero'
+        });
+      }
+      if (!('discontinuitySequence' in this.manifest)) {
+        this.manifest.discontinuitySequence = 0;
+        this.trigger('info', {
+          message: 'defaulting discontinuity sequence to zero'
+        });
+      }
+      if (entry.duration >= 0) {
+        this.currentUri.duration = entry.duration;
+      }
+      this.manifest.segments = this.uris;
+      break;
+    case 'key':
+      if (!entry.attributes) {
+        this.trigger('warn', {
+          message: 'ignoring key declaration without attribute list'
+        });
+        return;
+      }
+      // clear the active encryption key
+      if (entry.attributes.METHOD === 'NONE') {
+        this.key = null;
+        return;
+      }
+      if (!entry.attributes.URI) {
+        this.trigger('warn', {
+          message: 'ignoring key declaration without URI'
+        });
+        return;
+      }
+      if (!entry.attributes.METHOD) {
+        this.trigger('warn', {
+          message: 'defaulting key method to AES-128'
+        });
+      }
+
+      // setup an encryption key for upcoming segments
+      this.key = {
+        method: entry.attributes.METHOD || 'AES-128',
+        uri: entry.attributes.URI
+      };
+
+      if (typeof entry.attributes.IV !== 'undefined') {
+        this.key.iv = entry.attributes.IV;
+      }
+      break;
+    case 'media-sequence':
+      if (!isFinite(entry.number)) {
+        this.trigger('warn', {
+          message: 'ignoring invalid media sequence: ' + entry.number
+        });
+        return;
+      }
+      this.manifest.mediaSequence = entry.number;
+      break;
+    case 'discontinuity-sequence':
+      if (!isFinite(entry.number)) {
+        this.trigger('warn', {
+          message: 'ignoring invalid discontinuity sequence: ' + entry.number
+        });
+        return;
+      }
+      this.manifest.discontinuitySequence = entry.number;
+      break;
+    case 'playlist-type':
+      if (!(/VOD|EVENT/).test(entry.playlistType)) {
+        this.trigger('warn', {
+          message: 'ignoring unknown playlist type: ' + entry.playlist
+        });
+        return;
+      }
+      this.manifest.playlistType = entry.playlistType;
+      break;
+    case 'stream-inf':
+      this.manifest.playlists = this.uris;
+
+      if (!entry.attributes) {
+        this.trigger('warn', {
+          message: 'ignoring empty stream-inf attributes'
+        });
+        return;
+      }
+      if (!this.currentUri.attributes) {
+        this.currentUri.attributes = {};
+      }
+      this.currentUri.attributes = mergeOptions(this.currentUri.attributes,
+                                                entry.attributes);
+      break;
+    case 'discontinuity':
+      this.currentUri.discontinuity = true;
+      this.manifest.discontinuityStarts.push(this.uris.length);
+      break;
+    case 'targetduration':
+      if (!isFinite(entry.duration) || entry.duration < 0) {
+        this.trigger('warn', {
+          message: 'ignoring invalid target duration: ' + entry.duration
+        });
+        return;
+      }
+      this.manifest.targetDuration = entry.duration;
+      break;
+    case 'totalduration':
+      if (!isFinite(entry.duration) || entry.duration < 0) {
+        this.trigger('warn', {
+          message: 'ignoring invalid total duration: ' + entry.duration
+        });
+        return;
+      }
+      this.manifest.totalDuration = entry.duration;
+      break;
+    default:
+      break;
+    }
+  }
+
+  uri_(entry) {
+    this.currentUri.uri = entry.uri;
+    this.uris.push(this.currentUri);
+
+    // if no explicit duration was declared, use the target duration
+    if (this.manifest.targetDuration &&
+        !('duration' in this.currentUri)) {
+      this.trigger('warn', {
+        message: 'defaulting segment duration to the target duration'
+      });
+      this.currentUri.duration = this.manifest.targetDuration;
+    }
+    // annotate with encryption information, if necessary
+    if (this.key) {
+      this.currentUri.key = this.key;
+    }
+
+    // prepare for the next URI
+    this.currentUri = {};
+  }
+
+  comment_(entry) {
+    // comments are not important for playback
   }
 
   /**
