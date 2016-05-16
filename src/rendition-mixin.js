8181e3c46efd5d02af2bcd98c07096148d2c2457
jrivera <jrivera@brightcove.com>
2016-05-16 18:18:23 -0400

    Cleanup the rendition selection mixin to pass vjsstandard

--- src/rendition-mixin.js
+++ src/rendition-mixin.js
@@ -1,16 +1,32 @@
-export default function RenditionSelectionMixin(hlsHandler) {
-  let playlists = hlsHandler.playlists;
+let enableFunction = (playlist, changePlaylistFn, enable) => {
+  let currentlyEnabled = typeof playlist.excludeUntil === 'undefined' ||
+                         playlist.excludeUntil <= Date.now();
 
-  hlsHandler.representations = () => {
-    return playlists
-      .master
-      .playlists
-      .map((e, i) => new Representation(hlsHandler, e, i));
+  if (typeof enable === 'undefined') {
+    return currentlyEnabled;
   }
+
+  if (enable !== currentlyEnabled) {
+    if (enable) {
+      delete playlist.excludeUntil;
+    } else {
+      playlist.excludeUntil = Infinity;
+    }
+
+    // Ensure the outside world knows about our changes
+    changePlaylistFn();
+  }
+
+  return enable;
 };
 
 class Representation {
-  constructor (hlsHandler, playlist, id) {
+  constructor(hlsHandler, playlist, id) {
+    let fastChangeFunction = hlsHandler
+                              .masterPlaylistController_
+                              .fastQualityChange_
+                              .bind(hlsHandler.masterPlaylistController_);
+
     if (playlist.attributes) {
       let attributes = playlist.attributes;
 
@@ -23,28 +39,27 @@ class Representation {
 
       this.bandwidth = attributes.BANDWIDTH;
     }
+
+    // The id is simply the ordinality of the media playlist
+    // within the master playlist
     this.id = id;
 
-    this.enabled = (enable) => {
-      let currentlyEnabled = typeof playlist.excludeUntil === 'undefined' ||
-                             playlist.excludeUntil <= Date.now();
-
-      if (typeof enable === 'undefined') {
-        return currentlyEnabled;
-      }
-
-      if (enable !== currentlyEnabled) {
-        if (enable) {
-          delete playlist.excludeUntil;
-        } else {
-          playlist.excludeUntil = Infinity;
-        }
-
-        // Change quality as soon as possible
-        hlsHandler.masterPlaylistController_.fastQualityChange_();
-      }
-
-      return enable;
-    };
+    // Partially-apply the enableFunction to create a playlist-
+    // specific variant
+    this.enabled = enableFunction.bind(this, playlist, fastChangeFunction);
   }
 }
+
+let renditionSelectionMixin = function(hlsHandler) {
+  let playlists = hlsHandler.playlists;
+
+  // Add a single API-specific function to the HlsHandler instance
+  hlsHandler.representations = () => {
+    return playlists
+      .master
+      .playlists
+      .map((e, i) => new Representation(hlsHandler, e, i));
+  };
+};
+
+export default renditionSelectionMixin;
