30b05276a5057aa78c4e2a15867637d07fd739cc
Dylan Dove <ddove@brightcove.com>
2016-05-17 14:45:13 -0400

    Added some basic tests, going to flesh out

--- test/videojs-contrib-hls.test.js
+++ test/videojs-contrib-hls.test.js
@@ -2116,6 +2116,58 @@ QUnit.test('Allows overriding the global beforeRequest function', function() {
   delete videojs.Hls.xhr.beforeRequest;
 });
 
+QUnit.test('Adaptive seeking skips over gap in chrome without waiting event', function() {
+  let currentTime = 0;
+
+  this.player.autoplay(true);
+  this.player.buffered = function() {
+    return videojs.createTimeRanges([[0,10],[20,30]]);
+  };
+  this.player.src({
+    src: 'master.m3u8',
+    type: 'application/vnd.apple.mpegurl'
+  });
+
+  this.player.tech_.triggerReady();
+  this.clock.tick(1);
+  standardXHRResponse(this.requests.shift());
+  openMediaSource(this.player, this.clock);
+  this.player.tech_.trigger('play');
+  this.player.tech_.trigger('playing');
+  this.clock.tick(1);
+  this.player.currentTime(10);
+  for (let i = 0; i < 10; i++) {
+    this.player.trigger('timeupdate');
+  }
+  this.clock.tick(12000);
+  QUnit.equal(this.player.currentTime(), 20, 'Player seeked over gap');
+});
+
+QUnit.test('Adaptive seeking skips over gap in firefox with waiting event', function() {
+  let currentTime = 0;
+
+  this.player.autoplay(true);
+  this.player.buffered = function() {
+    return videojs.createTimeRanges([[0,10],[20,30]]);
+  };
+  this.player.src({
+    src: 'master.m3u8',
+    type: 'application/vnd.apple.mpegurl'
+  });
+
+  this.player.tech_.triggerReady();
+  this.clock.tick(1);
+  standardXHRResponse(this.requests.shift());
+  openMediaSource(this.player, this.clock);
+  this.player.tech_.trigger('play');
+  this.player.tech_.trigger('playing');
+  this.clock.tick(1);
+  this.player.currentTime(10);
+  this.player.trigger('waiting');
+  this.clock.tick(12000);
+  QUnit.equal(this.player.currentTime(), 20, 'Player seeked over gap');
+});
+
 QUnit.module('HLS Integration', {
   beforeEach() {
     this.env = useFakeEnvironment();
