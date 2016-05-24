46a8897a063f9cbfd3b737617f1c8a1548783958
Gary Katsevman <git@gkatsev.com>
2016-05-23 22:09:18 -0400

    Use browserfiy-shim globally. This is because media-sources requires video.js but doesn't require browserify-shim itself, so, instead we need to make sure that we browserify-shim all the dependencies.

--- scripts/build-test.js
+++ scripts/build-test.js
@@ -5,7 +5,7 @@ var glob = require('glob');
 glob('test/**/*.test.js', function(err, files) {
   browserify(files)
     .transform('babelify')
-    .transform('browserify-shim')
+    .transform('browserify-shim', {global: true})
     .bundle()
     .pipe(fs.createWriteStream('dist-test/videojs-contrib-hls.js'));
 });
