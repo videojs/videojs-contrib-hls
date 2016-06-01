896a70762848b0a2f9870396afcd2d82e9d2864a
jrivera <jrivera@brightcove.com>
2016-06-01 14:21:56 -0400

    Added doc comments to manual rendition selection mixin

--- src/rendition-mixin.js
+++ src/rendition-mixin.js
@@ -1,3 +1,16 @@
+/**
+ * Enable/disable playlist function. It is intended to have the first two
+ * arguments partially-applied in order to create the final per-playlist
+ * function.
+ *
+ * @param {PlaylistLoader} playlist - The rendition or media-playlist
+ * @param {Function} changePlaylistFn - A function to be called after a
+ * playlist's enabled-state has been changed. Will NOT be called if a
+ * playlist's enabled-state is unchanged
+ * @param {Boolean=} enable - Value to set the playlist enabled-state to
+ * or if undefined returns the current enabled-state for the playlist
+ * @return {Boolean} The current enabled-state of the playlist
+ */
 let enableFunction = (playlist, changePlaylistFn, enable) => {
   let currentlyEnabled = typeof playlist.excludeUntil === 'undefined' ||
                          playlist.excludeUntil <= Date.now();
@@ -20,13 +33,23 @@ let enableFunction = (playlist, changePlaylistFn, enable) => {
   return enable;
 };
 
+/**
+ * The representation object encapsulates the publicly visible information
+ * in a media playlist along with a setter/getter-type function (enabled)
+ * for changing the enabled-state of a particular playlist entry
+ *
+ * @class Representation
+ */
 class Representation {
   constructor(hlsHandler, playlist, id) {
+    // Get a reference to a bound version of fastQualityChange_
     let fastChangeFunction = hlsHandler
                               .masterPlaylistController_
                               .fastQualityChange_
                               .bind(hlsHandler.masterPlaylistController_);
 
+    // Carefully descend into the playlist's attributes since most
+    // properties are optional
     if (playlist.attributes) {
       let attributes = playlist.attributes;
 
@@ -50,6 +73,12 @@ class Representation {
   }
 }
 
+/**
+ * A mixin function that adds the `representations` api to an instance
+ * of the HlsHandler class
+ * @param {HlsHandler} hlsHandler - An instance of HlsHandler to add the
+ * representation API into
+ */
 let renditionSelectionMixin = function(hlsHandler) {
   let playlists = hlsHandler.playlists;
 
