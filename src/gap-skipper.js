3d3233fdc2eb97534d930de8471b391f355ef441
Dylan Dove <ddove@ddove-retina-2016.vidmark.local>
2016-06-01 14:42:28 -0400

    Brandons stuff

--- src/gap-skipper.js
+++ src/gap-skipper.js
@@ -1,85 +1,84 @@
 import Ranges from './ranges';
 import videojs from 'video.js';
 
-export default class gapSkipper extends videojs.EventTarget {
+export default class gapSkipper {
   constructor(options) {
-    super();
-    if (options.tech.options_.playerId) {
-      let self = this;
-
-      this.player = videojs(options.tech.options_.playerId);
-      this.tech_ = options.tech;
-      this.seeking = false;
-      this.consecutiveUpdates = 0;
-      this.timer = null;
-      this.playerState = null;
-      this.lastRecordedTime = null;
-
-      this.player.on('timeupdate', function() {
-        if (self.player.paused()) {
-          return;
-        }
-        let currentTime = self.player.currentTime();
-
-        if (self.consecutiveUpdates === 5 &&
-            currentTime === self.lastRecordedTime) {
-          // trigger waiting
-          if (self.playerState !== 'waiting') {
-            self.consecutiveUpdates = 0;
-            self.playerState = 'waiting';
-            self.skipTheGap();
-          }
-        } else if (currentTime === self.lastRecordedTime) {
-          self.consecutiveUpdates++;
-        } else {
-          self.consecutiveUpdates = 0;
-          self.lastRecordedTime = currentTime;
-        }
-      });
-
-      // Don't listen for waiting while seeking
-      this.player.on('seeking', function() {
-        self.seeking = true;
-      });
-
-      // Listen for waiting when finished seeking
-      this.player.on('seeked', function() {
-        self.seeking = false;
-      });
-
-      this.player.on('playing', function() {
-        self.player.on('waiting', self.skipTheGap);
-      });
-
-      this.player.on('error', function() {
-        if (self.timer) {
-          clearTimeout(self.timer);
-        }
-      });
+    if (!options.tech.options_.playerId) {
+      return;
     }
+
+    this.player = videojs(options.tech.options_.playerId);
+    this.tech_ = options.tech;
+    this.seeking = false;
+    this.consecutiveUpdates = 0;
+    this.timer = null;
+    this.playerState = null;
+    this.lastRecordedTime = null;
+
+    this.player.on('timeupdate', () => {
+      if (this.player.paused()) {
+        return;
+      }
+      let currentTime = this.player.currentTime();
+
+      if (this.consecutiveUpdates === 5 &&
+          currentTime === this.lastRecordedTime) {
+        // trigger waiting
+        if (this.playerState !== 'waiting') {
+          this.consecutiveUpdates = 0;
+          this.playerState = 'waiting';
+          this.skipTheGap();
+        }
+      } else if (currentTime === this.lastRecordedTime) {
+        this.consecutiveUpdates++;
+      } else {
+        this.consecutiveUpdates = 0;
+        this.lastRecordedTime = currentTime;
+      }
+    });
+
+    // Don't listen for waiting while seeking
+    this.player.on('seeking', () => {
+      this.seeking = true;
+    });
+
+    // Listen for waiting when finished seeking
+    this.player.on('seeked', () => {
+      this.seeking = false;
+    });
+
+    this.player.on('playing', () => {
+      this.player.on('waiting', this.skipTheGap);
+    });
+
+    this.player.on('error', () => {
+      if (this.timer) {
+        clearTimeout(this.timer);
+      }
+    });
   }
 
   skipTheGap() {
-    let self = this;
 
-    if (!this.seeking) {
-      let buffered = this.player.buffered();
-      let currentTime = this.player.currentTime();
+    if (this.seeking) {
+      return;
+    }
+    let buffered = this.player.buffered();
+    let currentTime = this.player.currentTime();
 
-      if (buffered.length > 0) {
-        let nextRange = Ranges.findNextRange(buffered, currentTime);
+    if (buffered.length > 0) {
+      let nextRange = Ranges.findNextRange(buffered, currentTime);
 
-        if (nextRange.length > 0) {
-          let difference = nextRange.start(0) - currentTime;
+      if (nextRange.length > 0) {
+        let difference = nextRange.start(0) - currentTime;
 
-          this.timer = setTimeout(function() {
-            if (self.player.currentTime() === currentTime) {
-              // only seek if we still have not played
-              self.player.currentTime(nextRange.start(0) + Ranges.TIME_FUDGE_FACTOR);
-              self.playerState = 'playing';
-            }
-          }, difference * 1000);
-        }
+        this.timer = setTimeout(() => {
+          if (this.player.currentTime() === currentTime) {
+            // only seek if we still have not played
+            this.player.currentTime(nextRange.start(0) + Ranges.TIME_FUDGE_FACTOR);
+            this.playerState = 'playing';
+          }
+        }, difference * 1000);
       }
     }
   }
