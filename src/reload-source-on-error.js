216697c47474f731aa928c58a4b6103e5f9a5d3d
jrivera <jrivera@brightcove.com>
2016-11-09 19:05:21 -0500

    Immediately reload the source when an error is emitted by the player

---
+++ src/reload-source-on-error.js
@@ -0,0 +1,33 @@
+/**
+ * Reload the source when an error is detected as long as there
+ * wasn't an error previously within the last 30 seconds
+ */
+const reloadSourceOnError = function() {
+  const player = this;
+  let lastCalled = 0;
+  const reloadSource = function() {
+    let sourceObj = player.currentSource();
+    let seekTo = (player.duration() !== Infinity && player.currentTime()) || 0;
+
+    if (Date.now() - lastCalled < 30 * 1000) {
+      return;
+    }
+    lastCalled = Date.now();
+
+    if (seekTo) {
+      player.one('loadedmetadata', () => {
+        player.currentTime(seekTo);
+      });
+    }
+
+    player.src(sourceObj);
+    player.play();
+  };
+
+  player.on('error', reloadSource);
+  player.on('dispose', () => {
+      player.off('error', reloadSource);
+  });
+};
+
+export default reloadSourceOnError;
