a24d468d532ff6094ea004330d03b004bff67e28
Matthew Neil <mjneil@users.noreply.github.com>
2017-02-28 15:17:55 -0500

    do not timeout segment requests for non-master playlist source (#1032)

--- src/playlist-loader.js
+++ src/playlist-loader.js
@@ -239,13 +239,13 @@ const PlaylistLoader = function(srcUrl, hls, withCredentials) {
    * @return {Boolean} true if on lowest rendition
    */
   loader.isLowestEnabledRendition_ = function() {
-    let media = loader.media();
-
-    if (!media || !media.attributes) {
-      return false;
+    if (loader.master.playlists.length === 1) {
+      return true;
     }
 
-    let currentBandwidth = media.attributes.BANDWIDTH || 0;
+    let media = loader.media();
+
+    let currentBandwidth = media.attributes.BANDWIDTH || Number.MAX_VALUE;
 
     return (loader.master.playlists.filter((playlist) => {
       const enabled = isEnabled(playlist);
