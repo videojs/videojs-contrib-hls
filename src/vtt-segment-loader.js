836c5b445217972d6bb279983cefc25351f7d8dc
Garrett Singer <gesinger@gmail.com>
2017-03-01 11:40:00 -0500

    Fix cue time references in vtt buffered check

--- src/vtt-segment-loader.js
+++ src/vtt-segment-loader.js
@@ -219,8 +219,8 @@ export default class VTTSegmentLoader extends videojs.EventTarget {
       return videojs.createTimeRanges();
     }
 
-    let start = cues[0].start;
-    let end = cues[cues.length - 1].start;
+    let start = cues[0].startTime;
+    let end = cues[cues.length - 1].startTime;
 
     return videojs.createTimeRanges([[start, end]]);
   }
