14258e2a330bcecab313a1e86ded10db598eb6dc
jrivera <jrivera@brightcove.com>
2017-03-08 18:37:28 -0500

    Added a few more comments to each function explaining the parameters

--- src/media-segment-request.js
+++ src/media-segment-request.js
@@ -10,6 +10,9 @@ export const REQUEST_ERRORS = {
 /**
  * Turns segment byterange into a string suitable for use in
  * HTTP Range requests
+ *
+ * @param {Object} byterange - an object with two values defining the start and end
+ *                             of a byte-range
  */
 const byterangeStr = function(byterange) {
   let byterangeStart;
@@ -24,6 +27,8 @@ const byterangeStr = function(byterange) {
 
 /**
  * Defines headers for use in the xhr request for a particular segment.
+ *
+ * @param {Object} segment - a simplified copy of the segmentInfo object from SegmentLoader
  */
 const segmentXhrHeaders = function(segment) {
   let headers = {};
@@ -34,10 +39,13 @@ const segmentXhrHeaders = function(segment) {
   return headers;
 };
 
+/**
+ * Abort all requests that have not already returned data
+ *
+ * @param {Object} activeXhrs - an object that tracks all XHR requests
+ */
 const abortAll = (activeXhrs) => {
-  Object.keys(activeXhrs).forEach((xhrKey) => {
-    const xhr = activeXhrs[xhrKey];
-
+  activeXhrs.forEach((xhr) => {
     // only abort xhrs that haven't had a response
     if (!xhr.responseTime) {
       // set an aborted property so that we can correctly
@@ -48,6 +56,11 @@ const abortAll = (activeXhrs) => {
   });
 };
 
+/**
+ * Gather important bandwidth stats once a request has completed
+ *
+ * @param {Object} request - the XHR request from which to gather stats
+ */
 const getRequestStats = (request) => {
   return {
     bandwidth: request.bandwidth,
@@ -56,6 +69,12 @@ const getRequestStats = (request) => {
   };
 };
 
+/**
+ * If possible gather bandwidth stats as a request is in
+ * progress
+ *
+ * @param {Event} progressEvent - an event object from an XHR's progress event
+ */
 const getProgressStats = (progressEvent) => {
   const request = progressEvent.target;
   const roundTripTime = Date.now() - request.requestTime;
@@ -72,6 +91,13 @@ const getProgressStats = (progressEvent) => {
   return stats;
 };
 
+/**
+ * Handle all error conditions in one place and return an object
+ * with all the information
+ *
+ * @param {Anything} error - if non-null signals an error occured with the XHR
+ * @param {Object} request -  the XHR request that possibly generated the error
+ */
 const handleErrors = (error, request) => {
   const response = request.response;
 
@@ -105,6 +131,14 @@ const handleErrors = (error, request) => {
   return null;
 };
 
+/**
+ * Handle responses for key data and convert the key data to the correct format
+ * for the decryption step later
+ *
+ * @param {Object} segment - a simplified copy of the segmentInfo object from SegmentLoader
+ * @param {Function} finishProcessingFn - a callback to execute to continue processing
+ *                                        this request
+ */
 const handleKeyResponse = (segment, finishProcessingFn) => (error, request) => {
   const response = request.response;
   const errorObj = handleErrors(error, request);
@@ -133,24 +167,32 @@ const handleKeyResponse = (segment, finishProcessingFn) => (error, request) => {
   return finishProcessingFn(null, segment);
 };
 
+/**
+ * Handle init-segment responses
+ *
+ * @param {Object} segment - a simplified copy of the segmentInfo object from SegmentLoader
+ * @param {Function} finishProcessingFn - a callback to execute to continue processing
+ *                                        this request
+ */
 const handleInitSegmentResponse = (segment, finishProcessingFn) => (error, request) => {
   const errorObj = handleErrors(error, request);
 
   if (errorObj) {
     return finishProcessingFn(errorObj, segment);
   }
-
   segment.map.bytes = new Uint8Array(request.response);
-
-  if (!Array.isArray(segment.bandwidth)) {
-    segment.bandwidth = [request.bandwidth];
-  } else {
-    segment.bandwidth.push(request.bandwidth);
-  }
-
   return finishProcessingFn(null, segment);
 };
 
+/**
+ * Response handler for segment-requests being sure to set the correct
+ * property depending on whether the segment is encryped or not
+ * Also records and keeps track of stats that are used for ABR purposes
+ *
+ * @param {Object} segment - a simplified copy of the segmentInfo object from SegmentLoader
+ * @param {Function} finishProcessingFn - a callback to execute to continue processing
+ *                                        this request
+ */
 const handleSegmentResponse = (segment, finishProcessingFn) => (error, request) => {
   const errorObj = handleErrors(error, request);
 
@@ -168,15 +210,14 @@ const handleSegmentResponse = (segment, finishProcessingFn) => (error, request) 
   return finishProcessingFn(null, segment);
 };
 
-const maybeDecrypt = (decrypter, segment, callback) => {
-  // Keep track of when all of the requests have completed
-  segment.endOfAllRequests = Date.now();
-
-  // If the segment is not encrypted, just continue
-  if (!segment.encryptedBytes) {
-    return callback(null, segment);
-  }
-
+/**
+ * Decrypt the segment via the decryption web worker
+ *
+ * @param {WebWorker} decrypter - a WebWorker interface to AES-128 decryption routines
+ * @param {Object} segment - a simplified copy of the segmentInfo object from SegmentLoader
+ * @param {Function} doneFn - a callback that is executed after decryption has completed
+ */
+const decryptSegment = (decrypter, segment, doneFn) => {
   const decryptionId = 'segment-request-' + Math.random().toString(36);
   const decryptionHandler = (event) => {
     if (event.data.source === decryptionId) {
@@ -186,7 +227,7 @@ const maybeDecrypt = (decrypter, segment, callback) => {
       segment.bytes = new Uint8Array(decrypted.bytes,
                                      decrypted.byteOffset,
                                      decrypted.byteLength);
-      return callback(null, segment);
+      return doneFn(null, segment);
     }
   };
 
@@ -205,7 +246,18 @@ const maybeDecrypt = (decrypter, segment, callback) => {
   ]);
 };
 
-const waitForCompletion = (activeXhrs, decrypter, callback) => {
+/**
+ * This function waits for all XHRs to finish (with either success or failure)
+ * before continueing processing via it's callback. The function gathers errors
+ * from each request into a single errors array so that the error status for
+ * each request can be examined later.
+ *
+ * @param {Object} activeXhrs - an object that tracks all XHR requests
+ * @param {WebWorker} decrypter - a WebWorker interface to AES-128 decryption routines
+ * @param {Function} doneFn - a callback that is executed after all resources have been
+ *                            downloaded and any decryption completed
+ */
+const waitForCompletion = (activeXhrs, decrypter, doneFn) => {
   let errors = [];
   let count = 0;
 
@@ -220,27 +272,80 @@ const waitForCompletion = (activeXhrs, decrypter, callback) => {
     }
     count += 1;
 
-    if (count === Object.keys(activeXhrs).length) {
+    if (count === activeXhrs.length) {
+      // Keep track of when *all* of the requests have completed
+      segment.endOfAllRequests = Date.now();
+
       if (errors.length > 0) {
-        return callback(errors, segment);
+        return doneFn(errors, segment);
       }
-      return maybeDecrypt(decrypter, segment, callback);
+      if (segment.encryptedBytes) {
+        return decryptSegment(decrypter, segment, doneFn);
+      }
+      // Otherwise, everything is ready just continue
+      return doneFn(null, segment);
     }
   };
 };
 
-const handleProgress = (segment, callback) => (event) => {
+/**
+ * Simple progress event callback handler that gathers some states before
+ * executing a provided callback with the `segment` object
+ *
+ * @param {Object} segment - a simplified copy of the segmentInfo object from SegmentLoader
+ * @param {Function} progressFn - a callback that is executed each time a progress event is received
+ * @param {Event} event - the progress event object from XMLHttpRequest
+ */
+const handleProgress = (segment, progressFn) => (event) => {
   segment.stats = getProgressStats(event);
-  return callback(event, segment);
+  return progressFn(event, segment);
 };
 
 /**
- * load a specific segment from a request into the buffer
+ * Load all resources and does any processing necessary for a media-segment
  *
- * @private
+ * Features:
+ *   decrypts the media-segment if it has a key uri and an iv
+ *   aborts *all* requests if *any* one request fails
+ *
+ * The segment object, at minimum, has the following format:
+ * {
+ *   resolvedUri: String,
+ *   [byterange]: {
+ *     offset: Number,
+ *     length: Number
+ *   },
+ *   [key]: {
+ *     resolvedUri: String
+ *     [byterange]: {
+ *       offset: Number,
+ *       length: Number
+ *     },
+ *     iv: {
+ *       bytes: Uint32Array
+ *     }
+ *   },
+ *   [map]: {
+ *     resolvedUri: String,
+ *     [byterange]: {
+ *       offset: Number,
+ *       length: Number
+ *     },
+ *     [bytes]: Uint8Array
+ *   }
+ * }
+ * ...where [name] denotes optional properties
+ *
+ * @param {Function} xhr - an instance of the xhr wrapper in xhr.js
+ * @param {Object} xhrOptions - the base options to provide to all xhr requests
+ * @param {WebWorker} decryptionWorker - a WebWorker interface to AES-128 decryption routines
+ * @param {Object} segment - a simplified copy of the segmentInfo object from SegmentLoader
+ * @param {Function} progressFn - a callback that receives progress events from the main segment's xhr request
+ * @param {Function} doneFn - a callback that is executed only once all requests have succeeded or failed
+ * @returns {Function} a function that, when invoked, immediately aborts all outstanding requests
  */
 export const mediaSegmentRequest = (xhr, xhrOptions, decryptionWorker, segment, progressFn, doneFn) => {
-  const activeXhrs = {};
+  const activeXhrs = [];
   const finishProcessingFn = waitForCompletion(activeXhrs, decryptionWorker, doneFn);
 
   // optionally, request the decryption key
@@ -250,8 +355,9 @@ export const mediaSegmentRequest = (xhr, xhrOptions, decryptionWorker, segment, 
       responseType: 'arraybuffer'
     });
     const keyRequestCallback = handleKeyResponse(segment, finishProcessingFn);
+    const keyXhr = xhr(keyRequestOptions, keyRequestCallback);
 
-    activeXhrs.keyXhr = xhr(keyRequestOptions, keyRequestCallback);
+    activeXhrs.push(keyXhr);
   }
 
   // optionally, request the associated media init segment
@@ -263,8 +369,9 @@ export const mediaSegmentRequest = (xhr, xhrOptions, decryptionWorker, segment, 
       headers: segmentXhrHeaders(segment.map)
     });
     const initSegmentRequestCallback = handleInitSegmentResponse(segment, finishProcessingFn);
+    const initSegmentXhr = xhr(initSegmentOptions, initSegmentRequestCallback);
 
-    activeXhrs.initSegmentXhr = xhr(initSegmentOptions, initSegmentRequestCallback);
+    activeXhrs.push(initSegmentXhr);
   }
 
   const segmentRequestOptions = videojs.mergeOptions(xhrOptions, {
@@ -273,9 +380,10 @@ export const mediaSegmentRequest = (xhr, xhrOptions, decryptionWorker, segment, 
     headers: segmentXhrHeaders(segment)
   });
   const segmentRequestCallback = handleSegmentResponse(segment, finishProcessingFn);
+  const segmentXhr = xhr(segmentRequestOptions, segmentRequestCallback);
 
-  activeXhrs.segmentXhr = xhr(segmentRequestOptions, segmentRequestCallback);
-  activeXhrs.segmentXhr.addEventListener('progress', handleProgress(segment, progressFn));
+  segmentXhr.addEventListener('progress', handleProgress(segment, progressFn));
+  activeXhrs.push(segmentXhr);
 
   return () => abortAll(activeXhrs);
 };
