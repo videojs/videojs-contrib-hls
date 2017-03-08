026e78c53276c2e43b7575b5804c560930f0e6e7
jrivera <jrivera@brightcove.com>
2017-03-08 15:16:41 -0500

    Made control flow order more explicit

--- src/media-segment-request.js
+++ src/media-segment-request.js
@@ -34,7 +34,7 @@ const segmentXhrHeaders = function(segment) {
   return headers;
 };
 
-const aborter = (activeXhrs) => () => {
+const abortAll = (activeXhrs) => {
   Object.keys(activeXhrs).forEach((xhrKey) => {
     const xhr = activeXhrs[xhrKey];
 
@@ -72,48 +72,49 @@ const getProgressStats = (progressEvent) => {
   return stats;
 };
 
-const handleErrors = (callback) => (error, request) => {
+const handleErrors = (error, request) => {
   const response = request.response;
 
   if (!request.aborted && error) {
-    return callback({
+    return {
       status: request.status,
       message: 'HLS request errored at URL: ' + request.uri,
       code: REQUEST_ERRORS.FAILURE,
       xhr: request
-    }, request);
+    };
   }
 
   if (request.timedout) {
-    return callback({
+    return {
       status: request.status,
       message: 'HLS request timed-out at URL: ' + request.uri,
       code: REQUEST_ERRORS.TIMEOUT,
       xhr: request
-    }, request);
+    };
   }
 
   if (!response) {
-    return callback({
+    return {
       status: request.status,
       message: 'HLS request aborted at URL: ' + request.uri,
       code: REQUEST_ERRORS.ABORTED,
       xhr: request
-    }, request);
+    };
   }
 
-  return callback(error, request);
+  return null;
 };
 
-const handleKeyResponse = (segment, callback) => (error, request) => {
+const handleKeyResponse = (segment, finishProcessingFn) => (error, request) => {
   const response = request.response;
+  const errorObj = handleErrors(error, request);
 
-  if (error) {
-    return callback(error, segment);
+  if (errorObj) {
+    return finishProcessingFn(errorObj, segment);
   }
 
   if (response.byteLength !== 16) {
-    return callback({
+    return finishProcessingFn({
       status: request.status,
       message: 'Invalid HLS key at URL: ' + request.uri,
       code: REQUEST_ERRORS.FAILURE,
@@ -129,13 +130,16 @@ const handleKeyResponse = (segment, callback) => (error, request) => {
     view.getUint32(8),
     view.getUint32(12)
   ]);
-  return callback(null, segment);
+  return finishProcessingFn(null, segment);
 };
 
-const handleInitSegmentResponse = (segment, callback) => (error, request) => {
-  if (error) {
-    return callback(error, segment);
+const handleInitSegmentResponse = (segment, finishProcessingFn) => (error, request) => {
+  const errorObj = handleErrors(error, request);
+
+  if (errorObj) {
+    return finishProcessingFn(errorObj, segment);
   }
+
   segment.map.bytes = new Uint8Array(request.response);
 
   if (!Array.isArray(segment.bandwidth)) {
@@ -144,12 +148,14 @@ const handleInitSegmentResponse = (segment, callback) => (error, request) => {
     segment.bandwidth.push(request.bandwidth);
   }
 
-  return callback(null, segment);
+  return finishProcessingFn(null, segment);
 };
 
-const handleSegmentResponse = (segment, callback) => (error, request) => {
-  if (error) {
-    return callback(error, segment);
+const handleSegmentResponse = (segment, finishProcessingFn) => (error, request) => {
+  const errorObj = handleErrors(error, request);
+
+  if (errorObj) {
+    return finishProcessingFn(errorObj, segment);
   }
   segment.stats = getRequestStats(request);
 
@@ -159,49 +165,16 @@ const handleSegmentResponse = (segment, callback) => (error, request) => {
     segment.bytes = new Uint8Array(request.response);
   }
 
-  return callback(null, segment);
+  return finishProcessingFn(null, segment);
 };
 
-const maybeAbortAll = (abortAll, callback) => (error, segment) => {
-  if (error) {
-    abortAll();
-  }
-  return callback(error, segment);
-};
-
-const waitForCompletion = (abortAll, callback) => {
-  let errors = [];
-  let count = 0;
-
-  return (error, segment) => {
-    // errors have to be unshifted to make sure the original error - the one
-    // that resulted in several aborts - ends up at the start of the array for
-    // ease of use downstream
-    if (error) {
-      errors.unshift(error);
-    }
-    count += 1;
-
-    if (count === Object.keys(abortAll).length) {
-      if (errors.length > 0) {
-        return callback(errors, segment);
-      }
-      return callback(null, segment);
-    }
-  };
-};
-
-const maybeDecrypt = (decrypter, callback) => (error, segment) => {
-  if (error) {
-    return callback(error, segment);
-  }
-
+const maybeDecrypt = (decrypter, segment, callback) => {
   // Keep track of when all of the requests have completed
   segment.endOfAllRequests = Date.now();
 
   // If the segment is not encrypted, just continue
   if (!segment.encryptedBytes) {
-    return callback(error, segment);
+    return callback(null, segment);
   }
 
   const decryptionId = 'segment-request-' + Math.random().toString(36);
@@ -213,7 +186,7 @@ const maybeDecrypt = (decrypter, callback) => (error, segment) => {
       segment.bytes = new Uint8Array(decrypted.bytes,
                                      decrypted.byteOffset,
                                      decrypted.byteLength);
-      return callback(error, segment);
+      return callback(null, segment);
     }
   };
 
@@ -232,6 +205,30 @@ const maybeDecrypt = (decrypter, callback) => (error, segment) => {
   ]);
 };
 
+const waitForCompletion = (activeXhrs, decrypter, callback) => {
+  let errors = [];
+  let count = 0;
+
+  return (error, segment) => {
+    // errors have to be unshifted to make sure the original error - the one
+    // that resulted in several aborts - ends up at the start of the array for
+    // ease of use downstream
+    if (error) {
+      // If there are errors, we have to abort any outstanding requests
+      abortAll(activeXhrs);
+      errors.unshift(error);
+    }
+    count += 1;
+
+    if (count === Object.keys(activeXhrs).length) {
+      if (errors.length > 0) {
+        return callback(errors, segment);
+      }
+      return maybeDecrypt(decrypter, segment, callback);
+    }
+  };
+};
+
 const handleProgress = (segment, callback) => (event) => {
   segment.stats = getProgressStats(event);
   return callback(event, segment);
@@ -242,11 +239,9 @@ const handleProgress = (segment, callback) => (event) => {
  *
  * @private
  */
-export const mediaSegmentRequest = (xhr, xhrOptions, decryptionWorker, segment, progressFn, nextFn) => {
+export const mediaSegmentRequest = (xhr, xhrOptions, decryptionWorker, segment, progressFn, doneFn) => {
   const activeXhrs = {};
-  const abortAll = aborter(activeXhrs);
-  const processSegmentCallback = waitForCompletion(activeXhrs,
-    maybeDecrypt(decryptionWorker, nextFn));
+  const finishProcessingFn = waitForCompletion(activeXhrs, decryptionWorker, doneFn);
 
   // optionally, request the decryption key
   if (segment.key) {
@@ -254,10 +249,9 @@ export const mediaSegmentRequest = (xhr, xhrOptions, decryptionWorker, segment, 
       uri: segment.key.resolvedUri,
       responseType: 'arraybuffer'
     });
+    const keyRequestCallback = handleKeyResponse(segment, finishProcessingFn);
 
-    activeXhrs.keyXhr = xhr(keyRequestOptions,
-      handleErrors(handleKeyResponse(segment,
-        maybeAbortAll(abortAll, processSegmentCallback))));
+    activeXhrs.keyXhr = xhr(keyRequestOptions, keyRequestCallback);
   }
 
   // optionally, request the associated media init segment
@@ -268,10 +262,9 @@ export const mediaSegmentRequest = (xhr, xhrOptions, decryptionWorker, segment, 
       responseType: 'arraybuffer',
       headers: segmentXhrHeaders(segment.map)
     });
+    const initSegmentRequestCallback = handleInitSegmentResponse(segment, finishProcessingFn);
 
-    activeXhrs.initSegmentXhr = xhr(initSegmentOptions,
-      handleErrors(handleInitSegmentResponse(segment,
-        maybeAbortAll(abortAll, processSegmentCallback))));
+    activeXhrs.initSegmentXhr = xhr(initSegmentOptions, initSegmentRequestCallback);
   }
 
   const segmentRequestOptions = videojs.mergeOptions(xhrOptions, {
@@ -279,12 +272,10 @@ export const mediaSegmentRequest = (xhr, xhrOptions, decryptionWorker, segment, 
     responseType: 'arraybuffer',
     headers: segmentXhrHeaders(segment)
   });
+  const segmentRequestCallback = handleSegmentResponse(segment, finishProcessingFn);
 
-  activeXhrs.segmentXhr = xhr(segmentRequestOptions,
-    handleErrors(handleSegmentResponse(segment,
-      maybeAbortAll(abortAll, processSegmentCallback))));
-
+  activeXhrs.segmentXhr = xhr(segmentRequestOptions, segmentRequestCallback);
   activeXhrs.segmentXhr.addEventListener('progress', handleProgress(segment, progressFn));
 
-  return abortAll;
+  return () => abortAll(activeXhrs);
 };
