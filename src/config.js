8c419cc78054a2e36e62766cfa0fa32bc887fac3
Garrett Singer <gesinger@gmail.com>
2017-05-22 16:48:40 -0400

    Add forward buffer low water line before upswitching rendition

--- src/config.js
+++ src/config.js
@@ -2,5 +2,7 @@ export default {
   GOAL_BUFFER_LENGTH: 60,
   // A fudge factor to apply to advertised playlist bitrates to account for
   // temporary flucations in client bandwidth
-  BANDWIDTH_VARIANCE: 1.2
+  BANDWIDTH_VARIANCE: 1.2,
+  // How much of the buffer must be filled before we consider upswitching
+  BUFFER_LOW_WATER_LINE: 10
 };
