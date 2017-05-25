70c495b6ce2e30be0995e461c091a2f87646ba2c
Matthew Neil <mjneil@users.noreply.github.com>
2017-05-25 18:30:12 -0400

    ignore low-water-line for live playlists

--- test/master-playlist-controller.test.js
+++ test/master-playlist-controller.test.js
@@ -752,6 +752,8 @@ QUnit.test('re-triggers bandwidthupdate events on the tech', function(assert) {
   assert.equal(bandwidthupdateEvents, 2, 'triggered bandwidthupdate');
 });
 
+// TODO: Add test for ignoring low waterline in LIVE playback
+
 QUnit.test('switches to lower renditions immediately, higher dependent on buffer',
 function(assert) {
   this.masterPlaylistController.mediaSource.trigger('sourceopen');
@@ -772,7 +774,8 @@ function(assert) {
     return {
       attributes: {
         BANDWIDTH: nextPlaylistBandwidth
-      }
+      },
+      endList: true
     };
   };
   this.masterPlaylistController.masterPlaylistLoader_.media = (media) => {
@@ -780,7 +783,8 @@ function(assert) {
       return {
         attributes: {
           BANDWIDTH: currentPlaylistBandwidth
-        }
+        },
+        endList: true
       };
     }
     mediaChanges.push(media);
