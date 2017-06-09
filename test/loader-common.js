814207f41c0ff9bc70629a5aa1ce232b188543bd
Matthew Neil <mjneil@users.noreply.github.com>
2017-06-09 16:07:40 -0400

    time savings for real this time

--- test/loader-common.js
+++ test/loader-common.js
@@ -292,7 +292,7 @@ export const LoaderCommonFactory = (LoaderConstructor,
       };
       let bandwidthupdates = 0;
 
-      playlist1.attributes.BANDWIDTH = 12000;
+      playlist1.attributes.BANDWIDTH = 18000;
       playlist2.attributes.BANDWIDTH = 10000;
       playlist3.attributes.BANDWIDTH = 8888;
       playlist4.attributes.BANDWIDTH = 7777;
