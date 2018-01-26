df072aa4a56c513da669e16fe1a47226911e6df1
Tom Jenkinson <tjenkinson@users.noreply.github.com>
2018-01-26 23:25:03 +0100

    simplify resolve-url

--- src/resolve-url.js
+++ src/resolve-url.js
@@ -6,17 +6,9 @@ import URLToolkit from 'url-toolkit';
 import window from 'global/window';
 
 const resolveUrl = function(baseURL, relativeURL) {
-  // return early if we don't need to resolve
-  if ((/^[a-z]+:/i).test(relativeURL)) {
-    return relativeURL;
-  }
-
-  // if the base URL is relative then combine with the current location
-  if (!(/\/\//i).test(baseURL)) {
-    baseURL = URLToolkit.buildAbsoluteURL(window.location.href, baseURL);
-  }
-
-  return URLToolkit.buildAbsoluteURL(baseURL, relativeURL);
+  return URLToolkit.buildAbsoluteURL(
+    URLToolkit.buildAbsoluteURL(window.location.href, baseURL), relativeURL
+  );
 };
 
 export default resolveUrl;
